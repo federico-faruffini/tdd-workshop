9ba734fb49048212e82219d9d4a1f4e607073f84
Federico Faruffini <federico.faruffini@imebit.com>
2025-05-30 11:45:25 +0200

    Example 1

--- 01-WordCounter/WordCounter/WordCounter.cs
+++ 01-WordCounter/WordCounter/WordCounter.cs
@@ -2,5 +2,14 @@ namespace WordCounter;
 
 public class WordCounter
 {
-    
+    public int CountWords(string? input)
+    {
+        if (string.IsNullOrEmpty(input))
+        {
+            return 0;
+        }
+
+        var words = input.Split(' ');
+        return words.Length;
+    }
 }
--- 01-WordCounter/WordCounterTests/WordCounterTests.cs
+++ 01-WordCounter/WordCounterTests/WordCounterTests.cs
@@ -3,13 +3,58 @@ namespace WordCounter;
 public class WordCounterTests
 {
     [Fact]
-    public void Given_When_Then()
+    public void GivenTheStringIsNull_WhenCountWords_ThenReturnsZero()
     {
         // Arrange
+        string? input = null;
+        var wordCounter = new WordCounter();
 
         // Act
+        var result = wordCounter.CountWords(input);
 
         // Assert
-        Assert.True(true);
+        Assert.Equal(0, result);
+    }
+
+    [Fact]
+    public void GivenTheStringIsEmpty_WhenCountWords_ThenReturnsZero()
+    {
+        // Arrange
+        string input = "";
+        var wordCounter = new WordCounter();
+
+        // Act
+        var result = wordCounter.CountWords(input);
+
+        // Assert
+        Assert.Equal(0, result);
+    }
+
+    [Fact]
+    public void GivenTwoWordsInTheString_WhenCountWords_ThenReturnsTwo()
+    {
+        // Arrange
+        string input = "one two";
+        var wordCounter = new WordCounter();
+
+        // Act
+        var result = wordCounter.CountWords(input);
+
+        // Assert
+        Assert.Equal(2, result);
+    }
+    
+    
+    public void GivenTwoWordsInTheStringSeparatedByThreeSpaces_WhenCountWords_ThenReturnsTwo()
+    {
+        // Arrange
+        string input = "one   two";
+        var wordCounter = new WordCounter();
+
+        // Act
+        var result = wordCounter.CountWords(input);
+
+        // Assert
+        Assert.Equal(2, result);
     }
 }
